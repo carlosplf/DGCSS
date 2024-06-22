a04d8256f0fdeb585471de01618afe560c87c3b3
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2024-06-22 18:23:59 -0300

    Remove prints

--- utils/plot_centroids.py
+++ utils/plot_centroids.py
@@ -10,22 +10,18 @@ def plot_centroids(all_nodes, centroids):
         (list of list): all_nodes.
         (list of list): centroids (also nodes).
     """
-    pca = PCA(n_components=2)   
+    pca = PCA(n_components=2)
     all_nodes_pca = pca.fit_transform(all_nodes.detach().numpy())
 
     pca = PCA(n_components=2)
     centroids_pca = pca.fit_transform(centroids)
-
-    print(all_nodes_pca)
-    print("===========")
-    print(centroids_pca)
 
     all_nodes_x = [row[0] for row in all_nodes_pca]
     all_nodes_y = [row[1] for row in all_nodes_pca]
 
     centroids_x = [row[0] for row in centroids_pca]
     centroids_y = [row[1] for row in centroids_pca]
-    
+
     _plot(all_nodes_x, all_nodes_y, centroids_x, centroids_y)
 
     return None
@@ -36,8 +32,8 @@ def _plot(all_nodes_x, all_nodes_y, centroids_x, centroids_y):
 
     # Add all nodes to the plot
     plt.scatter(all_nodes_x, all_nodes_y)
-    
+
     # Add centroids nodes to the plot
     plt.scatter(centroids_x, centroids_y, c=['#FF0000'])
-    
+
     plt.savefig('test.png')
