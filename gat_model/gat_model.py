5296613737957cedb3e9f3f41d679b978a33a075
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2024-07-18 20:22:04 -0300

    Remove heads arg

--- gat_model/gat_model.py
+++ gat_model/gat_model.py
@@ -6,13 +6,13 @@ import torch.nn.functional as F
 # Creating a GAT Network.
 class GATLayer(torch.nn.Module):
 
-    def __init__(self, in_channels, hidden_channels, out_channels, heads=1):
+    def __init__(self, in_channels, hidden_channels, out_channels):
         super(GATLayer, self).__init__()
 
         self.gat_conv1 = GATConv(in_channels, hidden_channels,
-                                 heads=heads, add_self_loops=True)
-        self.gat_conv2 = GATConv(hidden_channels * heads, out_channels,
-                                 heads=1, add_self_loops=True)
+                                 add_self_loops=True)
+        self.gat_conv2 = GATConv(hidden_channels, out_channels,
+                                 add_self_loops=True)
 
     # IMPORTANT: Just the Att matrix from one layer is being returned by this method.
     def forward(self, x, edge_index, weights=None):
