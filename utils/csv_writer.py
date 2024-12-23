d319f56d1369bf1e55b4c50a9e2d55bda7d31356
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2024-12-23 13:30:08 -0300

    Add metrics to CSV file

--- utils/csv_writer.py
+++ utils/csv_writer.py
@@ -8,3 +8,12 @@ def write_loss(loss_log, filename="logs/default_output.csv"):
         writer = csv.writer(f)
         writer.writerow(header)
         writer.writerows(loss_log)
+
+
+def write_metrics(metrics_log, filename="logs/metrics_log.csv"):
+    header = ["epoch", "mod", "nmi", "ari"]
+
+    with open(filename, "w", encoding="UTF8") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(metrics_log)
